f04f6222cedd6fad689197d28925fd2b806704df
Hector <hector.keith91@gmail.com>
2022-02-07 14:37:17 -0500

    beginning to incorporate game logic with UI

--- rock-paper-scissors/js/script.js
+++ rock-paper-scissors/js/script.js
@@ -1,40 +1,71 @@
 let playerScore = 0;
 let compScore = 0;
-let playerChoice;
 let compChoice;
 let playerWin = false;
 let compWin = false;
+let playerChoice;
 
-const playerSelection = document.querySelector(".player-selection");
+const playerSelectionUI = document.querySelector(".player-selection");
+const compSelectionUI = document.querySelector(".comp-selection");
 const options = document.querySelectorAll(".option");
-selectionMade = false;
+const playerScoreUI = document.querySelector(".player-score");
+const compScoreUI = document.querySelector(".computer-score");
 
 function handleMouseEnter() {
   this.classList.add("hovering");
-  playerSelection.textContent = this.textContent;
+  playerSelectionUI.textContent = this.textContent;
 }
 
-function handleMouseLeave(){
+function handleMouseLeave() {
   this.classList.remove("hovering");
-  playerSelection.textContent = "";
+  playerSelectionUI.textContent = "";
 }
 
-function handleClick(){
+function handleClick() {
   this.classList.add("selected");
-  options.forEach(option => {
+  playerChoice = playerSelectionUI.textContent;
+  // console.log("player selection: " + playerSelectionUI.textContent);
+
+  options.forEach((option) => {
     option.removeEventListener("mouseenter", handleMouseEnter);
     option.removeEventListener("mouseleave", handleMouseLeave);
     option.removeEventListener("click", handleClick);
   });
-  selectionMade = true;
+
+  compChoice = Math.floor(Math.random() * compOptions.length);
+  compChoice = playerOptions[compOptions[compChoice]];
+  compSelectionUI.textContent = compChoice;
+  // console.log("Computer chose: " + compChoice);
+  EvaluateMatch(playerChoice, compChoice);
 }
 
-options.forEach((option) => {
+function MakeSelections() {
+  options.forEach((option) => {
     option.addEventListener("mouseenter", handleMouseEnter);
     option.addEventListener("mouseleave", handleMouseLeave);
     option.addEventListener("click", handleClick);
   });
+}
 
+function EvaluateMatch(playerMove, compMove){
+  console.log("evaluating match")
+  console.log("player chose:player" + playerMove)
+  console.log("comp chose")
+  switch (playerMove) {
+    case "ROCK":
+      playerChoseRock();
+    case "PAPER":
+      playerChosePaper();
+    case "SCISSORS":
+      playerChoseScissors();
+  }
+};
+
+playerScoreUI.textContent = playerScore;
+compScoreUI.textContent = compScore; 
+MakeSelections();
+
+//old logic
 const playerOptions = {
   r: "ROCK",
   p: "PAPER",
@@ -50,22 +81,22 @@ function displayScores() {
   console.log("Computer Score: " + compScore);
 }
 
+function playerChoseRock() {
+  if (compChoice === "PAPER") compScore++;
+  else if (compChoice === "SCISSORS") playerScore++;
+  else MakeSelections;
+}
+
 function playerChosePaper() {
-  if (compChoice === "SCISSORS") compWin = true;
-  else if (compChoice === "ROCK") playerWin = true;
-  else decideMatch();
+  if (compChoice === "SCISSORS") compScore++;
+  else if (compChoice === "ROCK") playerScore++;
+  else MakeSelections;
 }
 
 function playerChoseScissors() {
-  if (compChoice === "ROCK") compWin = true;
-  else if (compChoice === "PAPER") playerWins();
-  else decideMatch();
-}
-
-function playerChoseRock() {
-  if (compChoice === "PAPER") compWin = true;
-  else if (compChoice === "SCISSORS") playerWin = true;
-  else decideMatch();
+  if (compChoice === "ROCK") compScore++;
+  else if (compChoice === "PAPER") playerScore++;
+  else MakeSelections;
 }
 
 function decideMatch() {
